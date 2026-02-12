ce007f93b240f1b64d6af5fcec44e018ab48f931
Curt Bercht Neto <bercht@live.com>
2026-02-12 13:33:28 -0300

    Fix Tailwind content paths

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    "./app/views/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js",
-    "./app/assets/stylesheets/**/*.css",
-    "./app/components/**/*.{erb,rb}"
+    "../app/views/**/*.html.erb",
+    "../app/helpers/**/*.rb",
+    "../app/javascript/**/*.js",
+    "../app/assets/stylesheets/**/*.css",
+    "../app/components/**/*.{erb,rb}"
   ],
   theme: {
     extend: {}
